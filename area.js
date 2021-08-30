79b606a0d49b9f1475231ba0cbbc845e53281574
Akhileshsukumaran <akhileshs2017@gmail.com>
2021-08-30 21:23:15 +0530

    added feature to find area

---
+++ area.js
@@ -0,0 +1,30 @@
+var base = document.querySelector("#base-input");
+var height = document.querySelector("#height-input");
+var areaBtn = document.querySelector("#btn-area");
+var outputEle = document.querySelector("#output");
+
+function calculateAreaOfTriangle(a, b)
+{
+    var triangleArea = 1/2 * a * b;
+    return triangleArea;
+}
+
+function calculateArea()
+{
+
+
+    if((base.value) == "" || (height.value) == "")
+    {
+        outputEle.innerText = "Please enter both base and height of the triangle! ";
+    }
+    else
+    {
+        var triangleArea = calculateAreaOfTriangle(Number(base.value), Number(height.value));
+        var area = triangleArea;
+        outputEle.innerText = "The Area of the triangle is: " + area + " cm²";
+    }
+
+}
+    
+
+areaBtn.addEventListener("click", calculateArea);
